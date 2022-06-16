d7f66da08d3e72d1a613582fd29e5bc0ea1981d1
jorge rafael callejo flores <riskeconomics@gmail.com>
2022-06-16 13:06:42 -0400

    instalación de librerias del proyecto

---
+++ clase_02.R
@@ -0,0 +1,10 @@
+# install.packages("tidyverse")
+# install.packages("knitr")
+# install.packages("rmarkdown")
+# install.packages("kableExtra")
+
+library(tidyverse)
+library(knitr)
+library(rmarkdown)
+library(kableExtra)
+
