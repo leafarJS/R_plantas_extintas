adf795ab5bfaa506772fb8d48a1fa8bf3f8e7e58
jorge rafael callejo flores <riskeconomics@gmail.com>
2022-06-16 13:09:43 -0400

    dirección de las bases de datos

--- clase_02.R
+++ clase_02.R
@@ -8,3 +8,6 @@ library(knitr)
 library(rmarkdown)
 library(kableExtra)
 
+plants <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv")
+actions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv")
+threats <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv")
